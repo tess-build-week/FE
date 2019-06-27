aef3d3c99f147fa3d45d14f5e13c25bee18e1015
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-06-27 15:55:57 -0500

    MVPbaby

--- BuildWeek--FindPlanets/src/App.js
+++ BuildWeek--FindPlanets/src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     console.log("inside App Render")
     return (
-      <div>
+      <div className = "NavBar">
         
        <ul>
           
--- BuildWeek--FindPlanets/src/Components/Planets/Planets.js
+++ BuildWeek--FindPlanets/src/Components/Planets/Planets.js
@@ -6,10 +6,10 @@ function Planets(props){
     return(
         <div className ="PlanetsCard">
             <div className = "PlanetInfo">
-                <p><strong>Name:</strong>{props.planet.planetid}</p>
-                <p><strong>orbit_period:</strong>{props.planet.orbit_period}</p>
-                <p><strong>Mass</strong>{props.planet.planet_mass}</p>
-                <p><strong>radius:</strong>{props.planet.planet_radius}</p> 
+                <p><strong>Name: </strong>{props.planet.planetid}</p>
+                <p><strong>orbit_period: </strong>{props.planet.orbit_period}</p>
+                <p><strong>Mass: </strong>{props.planet.planet_mass}</p>
+                <p><strong>radius: </strong>{props.planet.planet_radius}</p> 
             </div>
         </div>
     )
